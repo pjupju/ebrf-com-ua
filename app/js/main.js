0ed1073994841f4a516b5828f0bd66d7b46a5983
pjupju <tkachenkoyuriy12@gmail.com>
2020-09-17 23:35:21 +0300

    add accordion

--- app/js/main.js
+++ app/js/main.js
@@ -1,29 +1,16 @@
-var acc = document.getElementsByClassName("conditions__accordion");
-var faq = document.getElementsByClassName("faq__accordion-question");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
-}
-for (i = 0; i < faq.length; i++) {
-    faq[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
-}
+$('.faq__accordion-title').click(function () {
+    if ($(this).parent().hasClass('open')) {
+      $(this).parent().find('.faq__accordion-text').slideUp('fast');
+      $(this).parent().removeClass('open');
+    } else {
+      $('.faq__accordion').find('.open').each(function (index, el) {
+        $(el).removeClass('open');
+        $(el).find('.faq__accordion-text').slideUp('fast');
+      })
+      $(this).parent().find('.faq__accordion-text').slideDown('fast');
+      $(this).parent().addClass('open');
+    }
+  });
 
 function openCompany(evt, companyName) {
     let i, tabcontent, tablinks;
